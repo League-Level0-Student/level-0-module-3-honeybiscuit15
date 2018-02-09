2629a6d34d14d55d044ba22c22c315fe8d532d68
tactreconx <league@Admins-Mac-mini.local>
2018-02-08 18:25:41 -0800

    Crazycatlady and birthday reminder
    2-8-18

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -9,23 +9,32 @@ import javax.swing.JOptionPane;
 public class BirthdayReminder {
 
 	public static void main(String[] args) {
-	
+
 		// 1. correct the birthdays for your family below
 		String momsBirthday = "January 24th";
 		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String myBirthday = "January 6th";
+		// 2. Find out which birthday the user wants and and store their response in a
+		// variable
+		String answer = JOptionPane.showInputDialog("Who's birthday would you like to know");
 
-		// 2. Find out which birthday the user wants and and store their response in a variable
-	
 		// 3. Print out what the user typed
-		
+		if (answer.equalsIgnoreCase("moms")) {
+			JOptionPane.showMessageDialog(null, momsBirthday);
+		} else if (answer.equalsIgnoreCase("dads")) {
+			JOptionPane.showMessageDialog(null, dadsBirthday);
+		} else if (answer.equalsIgnoreCase("mine")) {
+			JOptionPane.showMessageDialog(null, myBirthday);
+		} else {
+			JOptionPane.showMessageDialog(null, "Sorry, i don't remember that person's birthday!");
+		}
 		// 4. if user asked for "mom"
-			//print mom's birthday
+		// print mom's birthday
 		// 5. if user asked for "dad"
-			// print dad's birthday
+		// print dad's birthday
 		// 6. if user asked for your name
-			// print myBirthday
-		//7. otherwise print "Sorry, i don't remember that person's birthday!"
+		// print myBirthday
+		// 7. otherwise print "Sorry, i don't remember that person's birthday!"
 
-	} 
+	}
 }
--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,19 +5,29 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+		String response=
+JOptionPane.showInputDialog("how many cats do you have");
 		// 2. Convert their answer into an int
+		int number= Integer.parseInt(response);
+// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+if(number>2) {
+JOptionPane.showMessageDialog(null, "you're a crazy cat lady");
+}
+// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
+else if(number<3&&number>0){
+playVideo("https://www.youtube.com/watch?v=XyNlqQId-nk");
+}
 
-		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
-		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
-		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
-	}
+// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
+	else {
+		playVideo("https://www.youtube.com/watch?v=W_juM14WHNQ");
+		}
+	}	
 
 	static void playVideo(String videoURL) {
 		try {
