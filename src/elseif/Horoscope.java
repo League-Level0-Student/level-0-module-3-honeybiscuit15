121a9de48d8d6c0f28695f57e9e0a46c4f753d92
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-01 18:07:29 -0800

    horoscope ip

---
+++ src/elseif/Horoscope.java
@@ -0,0 +1,21 @@
+package elseif;
+public class Horoscope {
+
+JOptionPane.showInputDialog("Excuse me whats you're star sign");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,24 +4,51 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+Robot max = new Robot();
 		//3. Ask the user what color they would like the robot to draw
-		
-		//4. Use an if/else statement to set the pen color that the user requested
+for (int j = 0; j < 5.; j++) {
 
+String color= JOptionPane.showInputDialog(null, "What color would you like");
+		//4. Use an if/else statement to set the pen color that the user requested
+		if (color.equalsIgnoreCase("red")) {
+	max.setPenColor(255, 0, 0);
+}
+	else if (color.equalsIgnoreCase("blue")) {
+		max.setPenColor(0, 0, 255);	
+}
+	else if (color.equalsIgnoreCase("green")) {
+		max.setPenColor(0, 128, 0);	
+}
+	else if (color.equalsIgnoreCase("yellow")) {
+		max.setPenColor(255, 255, 0);	
+}	
+	
         //5. If the user doesn’t enter anything, choose a random color
+		else {
+			max.setRandomPenColor();	
+}
 
         //6. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
 		
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
+for (int i = 0; i < 5.; i++) {
+	max.setPenWidth(10);
+	max.penDown();
+	max.setSpeed(10);
+	max.move(200);
+	max.turn(90);
+}
+}
 	}
 }
+	
