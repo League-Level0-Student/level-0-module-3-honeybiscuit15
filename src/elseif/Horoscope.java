4a4c8aa990e8203aed86bed81c62e02f1b100611
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-08 18:34:47 -0800

    walkoffame

--- src/elseif/Horoscope.java
+++ src/elseif/Horoscope.java
@@ -1,13 +1,53 @@
 package elseif;
+
+import javax.swing.JOptionPane;
+
 public class Horoscope {
+public static void main(String[] args) {
 
-JOptionPane.showInputDialog("Excuse me whats you're star sign");
+	
+String answer = JOptionPane.showInputDialog(null,"Excuse me what's you're star sign");
+if(answer.equalsIgnoreCase("Aquarius")) {
+	JOptionPane.showMessageDialog(null, "haha");
+}
+else if(answer.equalsIgnoreCase("Pisces")) {
+	JOptionPane.showMessageDialog(null, "haha");
+}
+else if (answer.equalsIgnoreCase("aries")) {
+	JOptionPane.showMessageDialog(null, "haha");
+}
+else if(answer.equalsIgnoreCase("Taurus")) {
+	JOptionPane.showMessageDialog(null,"haha");
+}
 
-//
+else if(answer.equalsIgnoreCase("Gemini")) {
+	JOptionPane.showMessageDialog(null,"haha");
+}
+else if(answer.equalsIgnoreCase("Cancer")) {
+	JOptionPane.showMessageDialog(null,"haha");
+}
+else if(answer.equalsIgnoreCase("Leo")) {
+	JOptionPane.showMessageDialog(null,"haha");
+}
+else if(answer.equalsIgnoreCase("Virgo")) {
+	JOptionPane.showMessageDialog(null,"haha");
+}
+else if(answer.equalsIgnoreCase("Libra")) {
+	JOptionPane.showMessageDialog(null,"haha");
+}
+else if(answer.equalsIgnoreCase("Scorpio")) {
+	JOptionPane.showMessageDialog(null,"haha");
+}
+else if(answer.equalsIgnoreCase("Sagittarius")) {
+	JOptionPane.showMessageDialog(null,"haha");
+}
+else if(answer.equalsIgnoreCase("Capricorn")) {
+	JOptionPane.showMessageDialog(null,"haha");
+}
+else {
+	JOptionPane.showMessageDialog(null,"You are special");
 
-
-
-
+}
 
 
 
@@ -19,3 +59,6 @@ JOptionPane.showInputDialog("Excuse me whats you're star sign");
 
 
 }
+
+} 
+
--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -8,12 +8,25 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
-		Robot rob = new Robot();
+		Robot max = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
-		// 2. Make the robot draw a star shape. Hint: 144.
-
+max.setX(100);
+max.setSpeed(500);
+max.penDown();
+max.setRandomPenColor();
+max.hide();
+for (int j = 0; j < 15; j++) {
+// 2. Make the robot draw a star shape. Hint: 144.
+for (int i = 0; i < 6; i++) {
+max.move(30);
+max.turn(144);	
+}
+max.penUp();
+max.setAngle(90);
+max.move(50);
+max.penDown();
+}
 		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
